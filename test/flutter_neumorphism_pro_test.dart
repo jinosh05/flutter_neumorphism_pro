dcddd2f2b9974ab3f6e99d6d431fb93c21fa5d49
Jinosh05 <jinoshprabhuraj5@gmail.com>
2024-11-18 00:06:07 +0530

    Updated the Analysis Options and Environment

--- test/flutter_neumorphism_pro_test.dart
+++ test/flutter_neumorphism_pro_test.dart
@@ -1,9 +1,8 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_neumorphism_pro/flutter_neumorphism_pro.dart';
+import "package:flutter_neumorphism_pro/flutter_neumorphism_pro.dart";
+import "package:flutter_test/flutter_test.dart";
 
 void main() {
-  test('adds one to input values', () {
+  test("adds one to input values", () {
     final calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
